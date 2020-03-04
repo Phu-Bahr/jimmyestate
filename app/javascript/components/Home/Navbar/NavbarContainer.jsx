586fa1e32ba35526892a1d2c667d278caa33b0b0
Tony Phu <tphu118@gmail.com>
2020-03-04 09:11:53 -0500

    changed navbar font, and parallax pictures

--- app/javascript/components/Home/Navbar/NavbarContainer.jsx
+++ app/javascript/components/Home/Navbar/NavbarContainer.jsx
@@ -14,15 +14,9 @@ class NavbarContainer extends Component {
 
   render() {
     return (
-      <nav className="navbar navbar-expand-lg navbar-light site-header sticky-top py-4">
+      <nav className="navbar navbar-expand-lg navbar-light site-header sticky-top py-4 navbar-font-type">
         <div className="container-fluid" style={{ maxWidth: 1150 }}>
-          <div
-            className="navbar-font"
-            role="navigation"
-            onClick={this.scrollToTop}
-          >
-            JC REALTY
-          </div>
+          <div className="navbar-font">JC Realty</div>
 
           <button
             className="navbar-toggler"
@@ -56,7 +50,7 @@ class NavbarContainer extends Component {
                   aria-haspopup="true"
                   aria-expanded="false"
                 >
-                  FEATURED COMMUNITIES
+                  Featured Communities
                 </Link>
                 <div className="dropdown-menu" aria-labelledby="navbarDropdown">
                   <Link to="/" className="dropdown-item navbar-underline">
@@ -81,17 +75,17 @@ class NavbarContainer extends Component {
               </li>
               <li className="nav-item">
                 <Link to="/service" className="nav-link navbar-underline">
-                  SERVICES
+                  Services
                 </Link>
               </li>
               <li className="nav-item">
                 <Link to="/about" className="nav-link navbar-underline">
-                  ABOUT
+                  About
                 </Link>
               </li>
               <li className="nav-item">
                 <Link to="/contact" className="nav-link navbar-underline">
-                  CONTACT
+                  Contact
                 </Link>
               </li>
             </ul>
