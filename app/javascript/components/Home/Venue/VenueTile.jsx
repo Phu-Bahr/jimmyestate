16a725687cb0710d53d0329fd5f9cdd0b3b3ffd7
Tony Phu <tphu118@gmail.com>
2020-03-04 11:14:54 -0500

    some adjustments

--- app/javascript/components/Home/Venue/VenueTile.jsx
+++ app/javascript/components/Home/Venue/VenueTile.jsx
@@ -2,15 +2,6 @@ import React from "react";
 import { Link } from "react-router-dom";
 import EditVenue from "./EditVenue";
 
-// {/* <div class="card mb-3">
-//   <img src="..." class="card-img-top" alt="...">
-//   <div class="card-body">
-//     <h5 class="card-title">Card title</h5>
-//     <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-//     <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-//   </div>
-// </div> */}
-
 const VenueTile = props => {
   return (
     <div className="col-md-6 col-middle px-3 py-2">
