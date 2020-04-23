7a4f52d4064d67825535ba62293186fa5cb994e0
Tony Phu <tphu118@gmail.com>
2020-04-22 22:45:40 -0400

    underconstruction images

--- app/javascript/components/Home/Service/ServiceContainer.jsx
+++ app/javascript/components/Home/Service/ServiceContainer.jsx
@@ -9,7 +9,10 @@ class ServiceContainer extends Component {
   render() {
     return (
       <div>
-        <h1>Hi from Service Page</h1>
+        <div className="container text-center">
+          <h1>Services coming to you soon!</h1>
+          <img src="https://i0.wp.com/www.palmbeachcountycta.org/wp-content/uploads/2017/10/website-construction-graphic-4.jpg?fit=1024%2C453" />
+        </div>
       </div>
     );
   }
