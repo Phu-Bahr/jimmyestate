d4a5b2dab1eca7a6892a6fb7c517fcf42bbb5f7d
Tony Phu <tphu118@gmail.com>
2020-04-24 14:36:10 -0400

    added delete button and logic

--- app/javascript/components/Home/Portfolio/PortfolioContainer.jsx
+++ app/javascript/components/Home/Portfolio/PortfolioContainer.jsx
@@ -21,12 +21,17 @@ class PortfolioContainer extends Component {
     this.onChange = this.onChange.bind(this);
     this.onSubmit = this.onSubmit.bind(this);
     this.toggleRefreshKey = this.toggleRefreshKey.bind(this);
+    this.toggleRefreshKeyFalse = this.toggleRefreshKeyFalse.bind(this);
+    this.deleteEvent = this.deleteEvent.bind(this);
   }
 
   toggleRefreshKey(event) {
     this.setState({ refreshKey: true });
   }
 
+  toggleRefreshKeyFalse(event) {
+    this.setState({ refreshKey: false });
+  }
   onChange(event) {
     this.setState({ [event.target.name]: event.target.value });
   }
@@ -80,6 +85,30 @@ class PortfolioContainer extends Component {
       .catch(error => console.log(error.message));
   }
 
+  deleteEvent(id) {
+    const urls = `/api/v1/portfolios/${id}`;
+    const token = document.querySelector('meta[name="csrf-token"]').content;
+
+    fetch(urls, {
+      method: "DELETE",
+      headers: {
+        "X-CSRF-Token": token,
+        "Content-Type": "application/json"
+      }
+    })
+      .then(response => {
+        if (response.ok) {
+          return response;
+        } else {
+          let errorMessage = `${response.status} (${response.statusText})`,
+            error = new Error(errorMessage);
+          throw error;
+        }
+      })
+      .then(this.toggleRefreshKey)
+      .catch(error => console.log(error.message));
+  }
+
   componentDidMount() {
     fetch("/api/v1/portfolios")
       .then(response => {
@@ -101,12 +130,46 @@ class PortfolioContainer extends Component {
       .catch(error => console.log("error message =>", error.message));
   }
 
+  componentDidUpdate() {
+    if (this.state.refreshKey === true) {
+      fetch("/api/v1/portfolios")
+        .then(response => {
+          if (response.ok) {
+            return response;
+          } else {
+            let errorMessage = `${response.status} (${response.statusText})`,
+              error = new Error(errorMessage);
+            throw error;
+          }
+        })
+        .then(response => response.json())
+        .then(body => {
+          let newPortfolioData = body;
+          this.setState({
+            portfolioData: newPortfolioData
+          });
+        })
+        .then(this.toggleRefreshKeyFalse)
+        .catch(error => console.log("error message =>", error.message));
+    }
+  }
+
   render() {
     console.log("Portfolio state ==> ", this.state.portfolioData);
+    console.log("refresh key", this.state.refreshKey);
 
     let data = this.state.portfolioData;
 
     let displayActivePortfolio = data.map(element => {
+      let handleDelete = () => {
+        let result = confirm(
+          `Are you sure you want to delete ${element.streetnumber} ${element.street}`
+        );
+        if (result) {
+          this.deleteEvent(element.id);
+        }
+      };
+
       if (element.status === "Active") {
         return (
           <div key={element.id} className="col-md-4 col-middle px-3 py-2">
@@ -117,7 +180,7 @@ class PortfolioContainer extends Component {
                     className="portfolioImage card-img-top"
                     src={element.photo}
                   />
-                  <div className="portfolioTitle">SOLD</div>
+                  <div className="portfolioTitle">On Market</div>
                 </div>
               </div>
 
@@ -132,12 +195,28 @@ class PortfolioContainer extends Component {
                 <div>{`${element.city}, ${element.state} ${element.zip}`}</div>
               </div>
             </div>
+            <button
+              type="button"
+              className="btn btn-secondary"
+              onClick={handleDelete}
+            >
+              Delete Venue
+            </button>
           </div>
         );
       }
     });
 
     let displaySoldPortfolio = data.map(element => {
+      let handleDelete = () => {
+        let result = confirm(
+          `Are you sure you want to delete ${element.streetnumber} ${element.street}`
+        );
+        if (result) {
+          this.deleteEvent(element.id);
+        }
+      };
+
       if (element.status === "Sold") {
         return (
           <div key={element.id} className="col-md-4 col-middle px-3 py-2">
@@ -163,12 +242,27 @@ class PortfolioContainer extends Component {
                 <div>{`${element.city}, ${element.state} ${element.zip}`}</div>
               </div>
             </div>
+            <button
+              type="button"
+              className="btn btn-secondary"
+              onClick={handleDelete}
+            >
+              Delete Venue
+            </button>
           </div>
         );
       }
     });
 
     let displayRentalPortfolio = data.map(element => {
+      let handleDelete = () => {
+        let result = confirm(
+          `Are you sure you want to delete ${element.streetnumber} ${element.street}`
+        );
+        if (result) {
+          this.deleteEvent(element.id);
+        }
+      };
       if (element.status === "Rental") {
         return (
           <div key={element.id} className="col-md-4 col-middle px-3 py-2">
@@ -179,7 +273,7 @@ class PortfolioContainer extends Component {
                     className="portfolioImage card-img-top"
                     src={element.photo}
                   />
-                  <div className="portfolioTitle">SOLD</div>
+                  <div className="portfolioTitle">Rental</div>
                 </div>
               </div>
 
@@ -194,6 +288,13 @@ class PortfolioContainer extends Component {
                 <div>{`${element.city}, ${element.state} ${element.zip}`}</div>
               </div>
             </div>
+            <button
+              type="button"
+              className="btn btn-secondary"
+              onClick={handleDelete}
+            >
+              Delete Venue
+            </button>
           </div>
         );
       }
