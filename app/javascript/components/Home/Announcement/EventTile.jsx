4c2cbab6e7165ab21eedd25a0cc27b102e3b6a9b
Tony Phu <tphu118@gmail.com>
2020-05-14 00:01:31 -0400

    onclick needed to be localized, was collapsing edit menu

--- app/javascript/components/Home/Announcement/EventTile.jsx
+++ app/javascript/components/Home/Announcement/EventTile.jsx
@@ -8,99 +8,99 @@ const EventTile = props => {
 
   return (
     <React.Fragment>
-      <div onClick={props.payload}>
-        <div className="event-border p-3">
+      <div className="event-border p-3">
+        <div onClick={props.payload}>
           <div>{props.title}</div>
           <div>{props.location}</div>
           <div>
             {moment(props.date).format("MMMM Do YYYY")}, {convert(props.time)}
           </div>
+        </div>
 
-          <br />
-          <div className={"px-3" + " " + props.hide}>
-            <button
-              type="button"
-              className="btn btn-info"
-              onClick={props.payload}
+        <br />
+        <div className={"px-3" + " " + props.hide}>
+          <button
+            type="button"
+            className="btn btn-info"
+            onClick={props.payload}
+          >
+            Edit
+          </button>
+          <button
+            type="button"
+            className="btn btn-secondary"
+            onClick={props.handleDelete}
+          >
+            Delete
+          </button>
+          <div className={"py-4" + " " + props.hideUpdate}>
+            <form
+              onSubmit={event => {
+                props.submitUpdate(event);
+                event.target.reset();
+              }}
             >
-              Edit
-            </button>
-            <button
-              type="button"
-              className="btn btn-secondary"
-              onClick={props.handleDelete}
-            >
-              Delete
-            </button>
-            <div className={"py-4" + " " + props.hideUpdate}>
-              <form
-                onSubmit={event => {
-                  props.submitUpdate(event);
-                  event.target.reset();
-                }}
-              >
-                <div className="row">
-                  <div className="col-sm-6">
-                    <label>Title</label>
-                    <input
-                      type="text"
-                      name="title"
-                      id="title"
-                      className="form-control"
-                      onChange={props.onChange}
-                      value={props.titleState}
-                    />
-                  </div>
-                  <div className="col-sm-6">
-                    <label>Location</label>
-                    <input
-                      type="text"
-                      name="location"
-                      id="location"
-                      className="form-control"
-                      onChange={props.onChange}
-                      value={props.locationState}
-                    />
-                  </div>
-                  <div className="col-sm-12 col-lg-6">
-                    <label>Date of Event</label>
-                    <input
-                      type="date"
-                      name="date"
-                      id="date"
-                      className="form-control"
-                      onChange={props.onChange}
-                      value={props.dateState}
-                    />
-                  </div>
-                  <div className="col-sm-12 col-lg-6">
-                    <label>Time of Event</label>
-                    <input
-                      type="time"
-                      name="time"
-                      id="time"
-                      className="form-control"
-                      onChange={props.onChange}
-                      value={props.timeState}
-                    />
-                  </div>
-                  <div className="col-sm-6">
-                    <label>Image URL</label>
-                    <input
-                      type="text"
-                      name="flier"
-                      id="flier"
-                      className="form-control"
-                      onChange={props.onChange}
-                      value={props.flierState}
-                    />
-                  </div>
+              <div className="row">
+                <div className="col-sm-6">
+                  <label>Title</label>
+                  <input
+                    type="text"
+                    name="title"
+                    id="title"
+                    className="form-control"
+                    onChange={props.onChange}
+                    value={props.titleState}
+                  />
                 </div>
-                <button type="submit" className="btn custom-button">
-                  Submit Update
-                </button>
-              </form>
-            </div>
+                <div className="col-sm-6">
+                  <label>Location</label>
+                  <input
+                    type="text"
+                    name="location"
+                    id="location"
+                    className="form-control"
+                    onChange={props.onChange}
+                    value={props.locationState}
+                  />
+                </div>
+                <div className="col-sm-12 col-lg-6">
+                  <label>Date of Event</label>
+                  <input
+                    type="date"
+                    name="date"
+                    id="date"
+                    className="form-control"
+                    onChange={props.onChange}
+                    value={props.dateState}
+                  />
+                </div>
+                <div className="col-sm-12 col-lg-6">
+                  <label>Time of Event</label>
+                  <input
+                    type="time"
+                    name="time"
+                    id="time"
+                    className="form-control"
+                    onChange={props.onChange}
+                    value={props.timeState}
+                  />
+                </div>
+                <div className="col-sm-6">
+                  <label>Image URL</label>
+                  <input
+                    type="text"
+                    name="flier"
+                    id="flier"
+                    className="form-control"
+                    onChange={props.onChange}
+                    value={props.flierState}
+                  />
+                </div>
+              </div>
+              <button type="submit" className="btn custom-button">
+                Submit Update
+              </button>
+            </form>
           </div>
         </div>
       </div>
