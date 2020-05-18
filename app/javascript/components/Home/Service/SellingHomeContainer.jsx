3891228320f7120988d1b3db8e1dfcdc53fc5958
Tony Phu <tphu118@gmail.com>
2020-05-18 15:20:04 -0400

    refactored buying/selling components with new draft js

--- app/javascript/components/Home/Service/SellingHomeContainer.jsx
+++ app/javascript/components/Home/Service/SellingHomeContainer.jsx
@@ -1,138 +1,29 @@
 import React, { Component } from "react";
-import { EditorState, convertToRaw, convertFromRaw } from "draft-js";
-import { Editor } from "react-draft-wysiwyg";
-import { FadeIn, FadeInUp } from "../../Constants/Constants";
+import { FadeIn } from "../../Constants/Constants";
+import DraftJSContainer from "../../Constants/DraftJSComponent";
 
 class SellingHomeContainer extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      editorState: EditorState.createEmpty(),
-      content: null,
-      refreshKey: false,
-      readOnly: false
+      urlGET: "selling_contents"
     };
-    this.onSubmit = this.onSubmit.bind(this);
-  }
-
-  updateEditorState(editorState) {
-    const contentState = editorState.getCurrentContent();
-    this.saveContent(contentState);
-    console.log("content state raw", convertToRaw(contentState));
-    this.setState({ editorState });
-  }
-
-  saveContent = contentData => {
-    this.setState({
-      content: JSON.stringify(convertToRaw(contentData))
-    });
-  };
-
-  onSubmit(event) {
-    if (this.state.readOnly) {
-      alert("Can't save on Read Only");
-    } else {
-      event.preventDefault();
-      const urls = "/api/v1/selling_contents";
-      const { content } = this.state;
-
-      const body = {
-        content
-      };
-
-      const token = document.querySelector('meta[name="csrf-token"]').content;
-
-      fetch(urls, {
-        method: "POST",
-        headers: {
-          "X-CSRF-Token": token,
-          "Content-Type": "application/json"
-        },
-        body: JSON.stringify(body)
-      })
-        .then(response => {
-          if (response.ok) {
-            alert("Content has been saved");
-            return response.json();
-          }
-          throw new Error("Network response was not ok.");
-        })
-        .catch(error => console.log(error.message));
-    }
-  }
-
-  componentDidMount() {
-    fetch("/api/v1/selling_contents")
-      .then(response => {
-        if (response.ok) {
-          return response;
-        } else {
-          let errorMessage = `${response.status} (${response.statusText})`,
-            error = new Error(errorMessage);
-          throw error;
-        }
-      })
-      .then(response => response.json())
-      .then(rawContent => {
-        if (rawContent) {
-          this.setState({
-            editorState: EditorState.createWithContent(
-              convertFromRaw(
-                JSON.parse(rawContent[rawContent.length - 1].content)
-              )
-            )
-          });
-        } else {
-          this.setState({ editorState: EditorState.createEmpty() });
-        }
-      })
-      .catch(error => console.log("error message =>", error.message));
   }
 
   render() {
-    let adminToggle;
-    if (this.props.user.admin) {
-      adminToggle = (
-        <div className="container pb-5 pt-3">
-          <Editor
-            editorState={this.state.editorState}
-            wrapperClassName="wrapperClassName"
-            editorClassName="editorClassName"
-            onEditorStateChange={this.updateEditorState.bind(this)}
-            readOnly={false}
-          />
-          <div className="pt-3">
-            <button onClick={this.onSubmit}>Save your content</button>
-          </div>
-        </div>
-      );
-    } else {
-      adminToggle = (
-        <div className="container pb-5 pt-3">
-          <Editor
-            toolbarHidden
-            editorState={this.state.editorState}
-            wrapperClassName="wrapperClassName"
-            editorClassName="editorClassName"
-            onEditorStateChange={this.updateEditorState.bind(this)}
-            readOnly={true}
-            placeholder="Sign In to Admin to edit"
-          />
-        </div>
-      );
-    }
-
     return (
       <React.Fragment>
         <FadeIn>
-          <div className="parallaxSellingPage">
+          <div className="parallaxBuyingPage">
             <div className="container py-5">
-              <h1>Hi there</h1>
+              <h1>Hi there buying stuff here</h1>
               <h4>helloooo there</h4>
             </div>
           </div>
         </FadeIn>
-        <FadeIn>{adminToggle}</FadeIn>
+        <div>
+          <DraftJSContainer {...this.state} {...this.props} />
+        </div>
       </React.Fragment>
     );
   }
