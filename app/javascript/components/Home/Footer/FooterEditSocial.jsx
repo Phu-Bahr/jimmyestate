cef1b0778a4ee6cfd6f76524246b94a1dc75f4a9
Tony Phu <tphu118@gmail.com>
2020-06-13 23:19:22 -0400

    changed google icon to business and added color transformation logic on hover

--- app/javascript/components/Home/Footer/FooterEditSocial.jsx
+++ app/javascript/components/Home/Footer/FooterEditSocial.jsx
@@ -11,12 +11,7 @@ const FooterEditSocial = props => {
           target="blank"
           className="c-magic-icon"
         >
-          <div className="c-magic-icon__fill facebook">
-            <FontAwesomeIcon
-              icon={["fab", "facebook-f"]}
-              className="iconfont mt-2"
-            />
-          </div>
+          <div className="c-magic-icon__fill facebook"></div>
           <div className="c-magic-icon__curtain"></div>
           <div className="c-magic-icon__outline">
             <FontAwesomeIcon
@@ -27,12 +22,7 @@ const FooterEditSocial = props => {
         </Link>
 
         <Link to={`//` + props.twitter} target="blank" className="c-magic-icon">
-          <div className="c-magic-icon__fill twitter">
-            <FontAwesomeIcon
-              icon={["fab", "linkedin-in"]}
-              className="iconfont mt-2"
-            />
-          </div>
+          <div className="c-magic-icon__fill twitter"></div>
           <div className="c-magic-icon__curtain"></div>
           <div className="c-magic-icon__outline">
             <FontAwesomeIcon
@@ -47,12 +37,7 @@ const FooterEditSocial = props => {
           target="blank"
           className="c-magic-icon"
         >
-          <div className="c-magic-icon__fill instagram">
-            <FontAwesomeIcon
-              icon={["fab", "instagram"]}
-              className="iconfont mt-2"
-            />
-          </div>
+          <div className="c-magic-icon__fill instagram"></div>
           <div className="c-magic-icon__curtain"></div>
           <div className="c-magic-icon__outline">
             <FontAwesomeIcon
@@ -63,28 +48,19 @@ const FooterEditSocial = props => {
         </Link>
 
         <Link to={`//` + props.other} target="blank" className="c-magic-icon">
-          <div className="c-magic-icon__fill googleplus">
-            <FontAwesomeIcon
-              icon={["fab", "google-plus-g"]}
-              className="iconfont mt-2"
-            />
-          </div>
+          <div className="c-magic-icon__fill googleplus"></div>
           <div className="c-magic-icon__curtain"></div>
           <div className="c-magic-icon__outline">
-            <FontAwesomeIcon
-              icon={["fab", "google-plus-g"]}
-              className="iconfont mt-2"
+            <img
+              className="google-icon-color"
+              style={{ height: "80%" }}
+              src="https://www.freepngimg.com/thumb/google/66984-logo-search-google-my-business-free-transparent-image-hq.png"
             />
           </div>
         </Link>
 
         <Link to={`//` + props.zillow} target="blank" className="c-magic-icon">
-          <div className="c-magic-icon__fill zillow">
-            <img
-              style={{ height: "80%", filter: "invert(100%)" }}
-              src="https://img.pngio.com/zillow-icon-png-91-images-in-collection-page-2-zillow-icon-png-512_512.png"
-            />
-          </div>
+          <div className="c-magic-icon__fill zillow"></div>
           <div className="c-magic-icon__curtain"></div>
           <div className="c-magic-icon__outline">
             <img
@@ -95,12 +71,7 @@ const FooterEditSocial = props => {
         </Link>
 
         <Link to={`//` + props.realtor} target="blank" className="c-magic-icon">
-          <div className="c-magic-icon__fill realtor">
-            <img
-              style={{ height: "90%", filter: "invert(100%)" }}
-              src="https://cdn3.iconfinder.com/data/icons/real-estate-glyph-8/100/Artboard_43-512.png"
-            />
-          </div>
+          <div className="c-magic-icon__fill realtor"></div>
           <div className="c-magic-icon__curtain"></div>
           <div className="c-magic-icon__outline">
             <img
