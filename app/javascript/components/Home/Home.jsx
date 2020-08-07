2890e953fc82ae5a1ee12b5d052a19cf1ecb9dab
Tony Phu <tphu118@gmail.com>
2020-08-07 09:43:41 -0400

    home refactor

--- app/javascript/components/Home/Home.jsx
+++ app/javascript/components/Home/Home.jsx
@@ -1,57 +1,23 @@
-import React, { Component } from "react";
+import React from "react";
 import CardsContainer from "../Home/Cards/CardsContainer";
 import JumbotronContainer from "../Home/Jumbotron/JumbotronContainer";
 import AnnouncementContainer from "../Home/Announcement/AnnouncementContainer";
-import ScrollAnimation from "react-animate-on-scroll";
 import EventContainer from "../Home/Events/EventContainer";
+import ScrollAnimation from "react-animate-on-scroll";
 
-class Home extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-    this.handleLogoutClick = this.handleLogoutClick.bind(this);
-  }
+const Home = props => {
+  return (
+    <div>
+      <JumbotronContainer {...props} />
 
-  handleLogoutClick() {
-    const url = "/logout";
+      <ScrollAnimation animateIn="fadeIn">
+        <AnnouncementContainer {...props} />
+      </ScrollAnimation>
+      <EventContainer user={props.user} />
 
-    fetch(url, {
-      method: "DELETE",
-      credentials: "include"
-    })
-      .then(response => {
-        this.props.handleLogout();
-      })
-      .catch(error => {
-        console.log("logout error", error);
-      });
-  }
-
-  render() {
-    let hideEditButton;
-    if (this.props.user.admin === true) {
-      hideEditButton = "";
-    } else {
-      // set below to blank string from invisibleto default show edit buttons
-      hideEditButton = "invisible";
-    }
-
-    return (
-      <div>
-        <JumbotronContainer {...this.props} />
-
-        <ScrollAnimation animateIn="fadeIn">
-          <AnnouncementContainer
-            hideEditButton={hideEditButton}
-            {...this.props}
-          />
-        </ScrollAnimation>
-        <EventContainer user={this.props.user} />
-
-        <CardsContainer hideEditButton={hideEditButton} {...this.props} />
-      </div>
-    );
-  }
-}
+      <CardsContainer {...props} />
+    </div>
+  );
+};
 
 export default Home;
