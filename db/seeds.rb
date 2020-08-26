145df5d7b610867d887724dc550f7b686fc23a63
Tony Phu <tphu118@gmail.com>
2020-08-26 00:59:23 -0400

    seed update

--- db/seeds.rb
+++ db/seeds.rb
@@ -20,21 +20,11 @@ footer = Footer.create!(
     realtor: 'www.realtor.com'
 )
 
-company = Company.create!(
-    description: "this is the beginning of a company description",
-    title: "edit your title here"
-)
-
 announcement = Announcement.create!(
     description: "hello, we are going to party soon", 
-    flier: "https://images.pexels.com/photos/207983/pexels-photo-207983.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
     title: "place title here",
     bannerImage: "test"
 )
-
-event = Event.create!([
-    { title: "Natick Open House", date: Date.parse('2020-12-25'), time: "10:00", location: "Natick" }
-])
 
 about = About.create!(
    bannerText1: "banner 1",
@@ -53,31 +43,6 @@ about = About.create!(
    photoemail: "john@aol.com",
    photoaddress1: "21 jump st",
    photoaddress2: "Boston, MA 02111"
-)
-
-AboutCompany = AboutCompany.create!(
-   bannerText1: "banner 1",
-   bannerText2: "banner 2",
-   paragraph1: "paragraph 1",
-   paragraph2: "paragraph 2",
-   paragraph3: "paragraph 3",
-   paragraph4: "paragraph 4",
-   paragraph5: "paragraph 5",
-   paragraph6: "paragraph 6",
-   paragraph7: "paragraph 7",
-   paragraph8: "paragraph 8",
-   photo: "photo info here",
-   photoname: "john smith",
-   photonumber: "555-555-5555",
-   photoemail: "john@aol.com",
-   photoaddress1: "21 jump st",
-   photoaddress2: "Boston, MA 02111"
-)
-
-townseed = TownLink.create!(
-    townlink: "www.example.com",
-    townlinkdescription: "Google Page",
-    town_id: 78
 )
 
 homeWorthEdit = WorthEdit.create!(
