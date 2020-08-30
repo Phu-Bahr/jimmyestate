b66cf8e358feaf99e2fffc85dc1bd4244d13bded
Tony Phu <tphu118@gmail.com>
2020-08-29 22:57:24 -0400

    footer css round 1

--- app/javascript/components/Footer/FooterEditContactUs.jsx
+++ app/javascript/components/Footer/FooterEditContactUs.jsx
@@ -3,20 +3,20 @@ import React from "react";
 const FooterEditContactUs = props => {
   return (
     <div className="pb-3">
-      <div className="d-flex justify-content-center">
+      <div className="footerAlignment">
         <a href={`tel:` + props.contact1} style={{ color: "black" }}>
           <div className="fa fa-phone pr-2" />
           {props.contact1}
         </a>
       </div>
-      <div className="d-flex justify-content-center">
+      <div className="footerAlignment">
         <a href={`mailto:` + props.contact2} style={{ color: "black" }}>
           <div className="fa fa-envelope pr-2" />
           {props.contact2}
         </a>
       </div>
-      <div className="d-flex justify-content-center">{props.contact3}</div>
-      <div className="d-flex justify-content-center">{props.contact4}</div>
+      <div className="footerAlignment">{props.contact3}</div>
+      <div className="footerAlignment">{props.contact4}</div>
     </div>
   );
 };
--- app/javascript/components/Footer/FooterEditSocial.jsx
+++ app/javascript/components/Footer/FooterEditSocial.jsx
@@ -1,87 +1,57 @@
 import React, { Fragment } from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const FooterEditSocial = props => {
   return (
     <Fragment>
-      <div className="d-flex justify-content-center mt-3">
-        <a
-          href={`//` + props.facebook}
-          target="_blank"
-          className="c-magic-icon"
-        >
-          <div className="c-magic-icon__fill facebook"></div>
-          <div className="c-magic-icon__curtain"></div>
-          <div className="c-magic-icon__outline">
-            <FontAwesomeIcon
-              icon={["fab", "facebook-f"]}
-              className="iconfont pt-2"
-            />
-          </div>
-        </a>
+      <div className="social-container">
+        <h3 style={{ fontWeight: "900" }}>SOCIAL</h3>
+        <ul className="social-icons">
+          <li>
+            <a href={`//` + props.facebook} target="_blank">
+              <i className="fa fa-facebook"></i>
+            </a>
+          </li>
 
-        <a href={`//` + props.twitter} target="_blank" className="c-magic-icon">
-          <div className="c-magic-icon__fill twitter"></div>
-          <div className="c-magic-icon__curtain"></div>
-          <div className="c-magic-icon__outline">
-            <FontAwesomeIcon
-              icon={["fab", "linkedin-in"]}
-              className="iconfont pt-2"
-            />
-          </div>
-        </a>
+          <li>
+            <a href={`//` + props.twitter} target="_blank">
+              <i className="fa fa-linkedin"></i>
+            </a>
+          </li>
 
-        <a
-          href={`//` + props.instagram}
-          target="_blank"
-          className="c-magic-icon"
-        >
-          <div className="c-magic-icon__fill instagram"></div>
-          <div className="c-magic-icon__curtain"></div>
-          <div className="c-magic-icon__outline">
-            <FontAwesomeIcon
-              icon={["fab", "instagram"]}
-              className="iconfont pt-2"
-            />
-          </div>
-        </a>
+          <li>
+            <a href={`//` + props.instagram} target="_blank">
+              <i className="fa fa-instagram"></i>
+            </a>
+          </li>
 
-        <a href={`//` + props.other} target="_blank" className="c-magic-icon">
-          <div className="c-magic-icon__fill googleplus"></div>
-          <div className="c-magic-icon__curtain"></div>
-          <div className="c-magic-icon__outline">
-            <img
-              className="google-icon-color"
-              style={{ height: "80%" }}
-              src="https://www.freepngimg.com/thumb/google/66984-logo-search-google-my-business-free-transparent-image-hq.png"
-              alt="google business icon"
-            />
-          </div>
-        </a>
+          <li>
+            <a href={`//` + props.other} target="_blank">
+              <img
+                id="google-icon"
+                src="https://www.freepngimg.com/thumb/google/66984-logo-search-google-my-business-free-transparent-image-hq.png"
+                alt="google business icon"
+              />
+            </a>
+          </li>
 
-        <a href={`//` + props.zillow} target="_blank" className="c-magic-icon">
-          <div className="c-magic-icon__fill zillow"></div>
-          <div className="c-magic-icon__curtain"></div>
-          <div className="c-magic-icon__outline">
-            <img
-              style={{ height: "80%", filter: "invert(100%)" }}
-              src="https://img.pngio.com/zillow-icon-png-91-images-in-collection-page-2-zillow-icon-png-512_512.png"
-              alt="zillow icon"
-            />
-          </div>
-        </a>
+          <li>
+            <a href={`//` + props.zillow} target="_blank">
+              <img
+                src="https://img.pngio.com/zillow-icon-png-91-images-in-collection-page-2-zillow-icon-png-512_512.png"
+                alt="zillow icon"
+              />
+            </a>
+          </li>
 
-        <a href={`//` + props.realtor} target="_blank" className="c-magic-icon">
-          <div className="c-magic-icon__fill realtor"></div>
-          <div className="c-magic-icon__curtain"></div>
-          <div className="c-magic-icon__outline">
-            <img
-              style={{ height: "90%", filter: "invert(100%)" }}
-              src="https://cdn3.iconfinder.com/data/icons/real-estate-glyph-8/100/Artboard_43-512.png"
-              alt="realtor house icon"
-            />
-          </div>
-        </a>
+          <li>
+            <a href={`//` + props.realtor} target="_blank">
+              <img
+                src="https://cdn3.iconfinder.com/data/icons/real-estate-glyph-8/100/Artboard_43-512.png"
+                alt="realtor house icon"
+              />
+            </a>
+          </li>
+        </ul>
       </div>
     </Fragment>
   );
