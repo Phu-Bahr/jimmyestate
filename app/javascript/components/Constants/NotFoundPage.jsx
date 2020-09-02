6301bb4e15c16162b38ff4c24ef0d9fefce2e4d5
Tony Phu <tphu118@gmail.com>
2020-09-02 13:50:52 -0400

    404 page

--- app/javascript/components/Constants/NotFoundPage.jsx
+++ app/javascript/components/Constants/NotFoundPage.jsx
@@ -1,11 +1,22 @@
 import React, { Fragment } from "react";
-import { Link } from "react-router-dom";
 
 const NotFoundPage = () => {
   return (
     <Fragment>
-      <div className="not-found-page">404 not found</div>;
-      <Link to="/">Go to Home</Link>
+      <div className="mainbox-background">
+        <div className="mainbox">
+          <div className="err">4</div>
+          <i className="far fa-question-circle fa-spin"></i>
+          <div className="err2">4</div>
+          <div className="msg">
+            Maybe this page moved? Got deleted? Is hiding out in quarantine?
+            Never existed in the first place?
+            <p>
+              Let's go <a href="/">home</a> and try from there.
+            </p>
+          </div>
+        </div>
+      </div>
     </Fragment>
   );
 };
