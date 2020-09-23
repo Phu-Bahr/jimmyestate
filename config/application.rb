f849eb4485f5a7b037c37ca97a1f6cfc2b407d9b
Tony Phu <tphu118@gmail.com>
2020-09-23 15:03:51 -0400

    application module name change and load_defaults to 6.0

--- config/application.rb
+++ config/application.rb
@@ -6,10 +6,10 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module ThirstyThursdays
+module JimmyEstates
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.2
+    config.load_defaults 6.0
     config.middleware.use ActionDispatch::Cookies
     config.middleware.use ActionDispatch::Session::CookieStore
     # Settings in config/environments/* take precedence over those specified here.
