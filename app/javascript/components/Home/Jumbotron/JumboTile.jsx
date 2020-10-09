a0b262b2d96cffff8bbc7bc6e721d2859199f9fc
Tony Phu <tphu118@gmail.com>
2020-10-08 23:33:12 -0400

    fixed heading animation jitter. used absolute instead of relative so its not the whole row coming in but the h1 div its inside in.

--- app/javascript/components/Home/Jumbotron/JumboTile.jsx
+++ app/javascript/components/Home/Jumbotron/JumboTile.jsx
@@ -2,13 +2,11 @@ import React from "react";
 
 const JumboTile = props => {
   return (
-    <header>
-      <h1 className="jumboFont-primary">
-        <span className="jumboFont1">{props.line1}</span>
-        <span className="jumboFont2">{props.line2}</span>
-        <span className="jumboFont3">{props.line3}</span>
-      </h1>
-    </header>
+    <h1 className="jumboFont-primary">
+      <span className="jumboFont1">{props.line1}</span>
+      <span className="jumboFont2">{props.line2}</span>
+      <span className="jumboFont3">{props.line3}</span>
+    </h1>
   );
 };
 
