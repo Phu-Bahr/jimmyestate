333ade8324ac18edd83805b6a1268b0703721d7d
Tony Phu <tphu118@gmail.com>
2020-10-14 13:41:33 -0400

    got rid of paragraphs

--- app/javascript/components/Home/Boxes/BoxContainer.jsx
+++ app/javascript/components/Home/Boxes/BoxContainer.jsx
@@ -15,33 +15,23 @@ class BoxContainer extends Component {
               <div className="feature-box">
                 <i className="feature-box__icon icon-basic-spread-text"></i>
                 <h2 className="feature-box__heading">Get Educated</h2>
-                <p className="feature-box__text">Trust me and my experience.</p>
               </div>
             </div>
             <div className="col-sm-12 col-md-6 col-lg-4 py-3 my-auto">
               <div className="feature-box">
-                {/* <i className="feature-box__icon icon-basic-cup"></i> */}
-
                 <div className="feature-box__icon ">
                   <img
                     src="https://icon-library.net//images/confidence-icon/confidence-icon-20.jpg"
                     className="confidentIcon"
                   />
                 </div>
-
                 <h2 className="feature-box__heading">Feel Confident</h2>
-                <p className="feature-box__text">
-                  Trust me and my fellow clients.
-                </p>
               </div>
             </div>
             <div className="col-sm-12 col-md-6 col-lg-4 py-3 my-auto">
               <div className="feature-box">
                 <i className="feature-box__icon icon-basic-joypad"></i>
                 <h2 className="feature-box__heading">Be in Control</h2>
-                <p className="feature-box__text">
-                  Trust me with all your needs.
-                </p>
               </div>
             </div>
           </div>
