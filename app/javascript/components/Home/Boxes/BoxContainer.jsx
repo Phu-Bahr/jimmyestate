c3346e1f1f29ddac38c00130a785f742f75e7ffd
Tony Phu <tphu118@gmail.com>
2020-10-14 00:00:32 -0400

    changing icon file to scss. heroku is pulling icon asset

--- app/javascript/components/Home/Boxes/BoxContainer.jsx
+++ app/javascript/components/Home/Boxes/BoxContainer.jsx
@@ -8,33 +8,37 @@ class BoxContainer extends Component {
 
   render() {
     return (
-      <div className="container p-5">
-        <div className="row">
-          <div className="col-sm-12 col-md-12 col-lg-4 py-3 ">
-            <div className="feature-box">
-              <i className="feature-box__icon icon-basic-spread-text"></i>
-              <h2 className="feature-box__heading">Get Educated</h2>
-              <p className="feature-box__text">Trust me and my experience.</p>
+      <section className="section-features">
+        <div className="container h-100 p-5">
+          <div className="row h-100">
+            <div className="col-sm-12 col-md-12 col-lg-4 py-3 my-auto">
+              <div className="feature-box">
+                <i className="feature-box__icon icon-basic-spread-text"></i>
+                <h2 className="feature-box__heading">Get Educated</h2>
+                <p className="feature-box__text">Trust me and my experience.</p>
+              </div>
             </div>
-          </div>
-          <div className="col-sm-12 col-md-6 col-lg-4 py-3">
-            <div className="feature-box">
-              <i className="feature-box__icon icon-basic-cup"></i>
-              <h2 className="feature-box__heading">Feel Confident</h2>
-              <p className="feature-box__text">
-                Trust me and my fellow clients.
-              </p>
+            <div className="col-sm-12 col-md-6 col-lg-4 py-3 my-auto">
+              <div className="feature-box">
+                <i className="feature-box__icon icon-basic-cup"></i>
+                <h2 className="feature-box__heading">Feel Confident</h2>
+                <p className="feature-box__text">
+                  Trust me and my fellow clients.
+                </p>
+              </div>
             </div>
-          </div>
-          <div className="col-sm-12 col-md-6 col-lg-4 py-3">
-            <div className="feature-box">
-              <i className="feature-box__icon icon-basic-joypad"></i>
-              <h2 className="feature-box__heading">Be in Control</h2>
-              <p className="feature-box__text">Trust me with all your needs.</p>
+            <div className="col-sm-12 col-md-6 col-lg-4 py-3 my-auto">
+              <div className="feature-box">
+                <i className="feature-box__icon icon-basic-joypad"></i>
+                <h2 className="feature-box__heading">Be in Control</h2>
+                <p className="feature-box__text">
+                  Trust me with all your needs.
+                </p>
+              </div>
             </div>
           </div>
         </div>
-      </div>
+      </section>
     );
   }
 }
