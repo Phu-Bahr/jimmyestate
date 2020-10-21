b58f6e7f1f1351e3f15ad2b13774a594887aa339
Tony Phu <tphu118@gmail.com>
2020-10-20 21:23:19 -0400

    updated seed file for installation

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,9 @@
 jumbotron = Jumbotron.create!(
-    line1: 'Line 1!', 
-    line2: 'Line 2', 
-    line3: 'Line 3'
+    line1: 'Hello!', 
+    line2: 'Welcome to your OWN website', 
+    line3: 'Edit what you want',
+    image: 'https://images.pexels.com/photos/196667/pexels-photo-196667.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    opacity: '.55'
     )
 
 footer = Footer.create!(
@@ -21,14 +23,15 @@ footer = Footer.create!(
 )
 
 announcement = Announcement.create!(
-        bannerImage: "https://images.pexels.com/photos/421927/pexels-photo-421927.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
-        title: "place title here",
-        description: "hello, we are going to party soon"
+    bannerImage: "https://images.pexels.com/photos/421927/pexels-photo-421927.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    title: "place title here",
+    description: "hello, we are going to party soon"
 )
 
 about = About.create!(
-   bannerText1: "banner 1",
-   bannerText2: "banner 2",
+   bannerImage: "https://images.pexels.com/photos/5273144/pexels-photo-5273144.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500", 
+   bannerText1: "Your about header here",
+   bannerText2: "Edit me!",
    paragraph1: "paragraph 1",
    paragraph2: "paragraph 2",
    paragraph3: "paragraph 3",
@@ -36,9 +39,9 @@ about = About.create!(
    paragraph5: "paragraph 5",
    paragraph6: "paragraph 6",
    paragraph7: "paragraph 7",
-   paragraph8: "paragraph 8",
-   photo: "photo info here",
-   photoname: "john smith",
+   paragraph8: "John Smith",
+   photo: "https://images.pexels.com/photos/1805164/pexels-photo-1805164.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+   photoname: "John Smith",
    photonumber: "555-555-5555",
    photoemail: "john@aol.com",
    photoaddress1: "21 jump st",
@@ -46,25 +49,28 @@ about = About.create!(
 )
 
 homeWorthEdit = WorthEdit.create!(
-    paragraph1: "test", 
-    paragraph2: "test", 
-    bannerText1: "test", 
-    bannerText2: "test"
-)
-
-
+    paragraph1: "Home worth text 1", 
+    paragraph2: "Home worth text 1", 
+    bannerText1: "What is your home worth?", 
+    bannerText2: "Edit this header",
+    bannerImage: "https://images.pexels.com/photos/3740794/pexels-photo-3740794.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
+    )
+    
+    
 relocationEdit = RelocationEdit.create!(
-    paragraph1: "test", 
-    paragraph2: "test", 
-    bannerText1: "test", 
-    bannerText2: "test"
+    paragraph1: "Relocation info here", 
+    paragraph2: "More relocation info in this block", 
+    bannerText1: "Relocation Title", 
+    bannerText2: "Edit this header",
+    bannerImage: "https://images.pexels.com/photos/3616688/pexels-photo-3616688.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
 )
 
 aboutCompanySeed = AboutCompany.create!(
+    bannerImage: "https://images.pexels.com/photos/269077/pexels-photo-269077.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
     content: "test",
-    headerText1: "test",
-    headerText2: "test",
-    image: "test"
+    headerText1: "Your company",
+    headerText2: "Edit title here",
+    image: "https://images.pexels.com/photos/1337380/pexels-photo-1337380.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
 )
 
 venueEditSeed = VenueEdit.create!(
@@ -72,46 +78,100 @@ venueEditSeed = VenueEdit.create!(
 )
 
 venueTemplating = VenueTemplate.create!(
-        content: "test",
-        headerText1: "test",
-        headerText2: "test",
-        image: "test",
-        bannerImage: "test"
+    content: "test",
+    headerText1: "test",
+    headerText2: "test",
+    image: "https://images.pexels.com/photos/2847949/pexels-photo-2847949.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    bannerImage: "https://images.pexels.com/photos/1239162/pexels-photo-1239162.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
 )
 
 portfolioEdit = PortfolioEdit.create!(
-    bannerImage: "test",
-    headerText1: "test",
-    headerText2: "test"
+    bannerImage: "https://images.pexels.com/photos/164522/pexels-photo-164522.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    headerText1: "Show off success",
+    headerText2: "Edit banner!"
 )
 
 contactEdit = ContactEdit.create!(
-    bannerImage: "test",
-    headerText1: "test",
-    headerText2: "test",
-    name: "test",
-    address: "test",
-    phonenumber: "test",
-    email: "test",
-    lat: "test",
-    lng: "test"
+    bannerImage: "https://images.pexels.com/photos/6386/person-woman-hand-smartphone.jpg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    headerText1: "Contact me!",
+    headerText2: "Edit banner here",
+    name: "Mike Tyson",
+    address: "21 Jump St",
+    phonenumber: "555-555-5555",
+    email: "PunchOut@gmail.com",
+    lat: "48.8584",
+    lng: "2.2945"
 )
 
 marketReportSeed = MarketReportEdit.create!(
-    bannerImage: "test",
-    paragraph1: "test", 
-    paragraph2: "test", 
-    bannerText1: "test", 
-    bannerText2: "test"
+    bannerImage: "https://images.pexels.com/photos/106344/pexels-photo-106344.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    paragraph1: "Market info here", 
+    paragraph2: "Market info again", 
+    bannerText1: "Need market report?", 
+    bannerText2: "Edit header here"
 )
 
 testimonialEdit = TestimonialEdit.create!(
-    bannerImage: "test",
-    headerText1: "test",
-    headerText2: "test"
+    bannerImage: "https://images.pexels.com/photos/3847703/pexels-photo-3847703.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    headerText1: "Testimonials here",
+    headerText2: "Edit title!"
+)
+
+testimonialFirst = Testimonial.create!(
+    image: "https://images.pexels.com/photos/271816/pexels-photo-271816.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    title: "Best Person to work with!",
+    description: "I had a grand old time with the entire process. Would do it again in a heart beat and tell all my friends about it. Please try this person out!",
+    name: "Bob - Boston, MA"
 )
 
 customCards = CustomCard.create!([
-        { image: "test1", title: "test1" },
-        { image: "test2", title: "test2" }
+    { image: "https://images.pexels.com/photos/463996/pexels-photo-463996.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500", title: "Featured Communities" },
+    { image: "https://images.pexels.com/photos/814544/pexels-photo-814544.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500", title: "Partners" }
 ])
+
+cardDrafts = CardDraft.create!(
+    content: "test"
+)
+
+buyingSection = BuyingContent.create!(
+    bannerImage: "https://images.pexels.com/photos/34577/pexels-photo.jpg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    headerText1: "Buying tips here",
+    headerText2: "Edit header",
+    content: "test"
+)
+
+sellingingSection = SellingContent.create!(
+    bannerImage: "https://images.pexels.com/photos/50987/money-card-business-credit-card-50987.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    headerText1: "Selling tips here",
+    headerText2: "Edit header",
+    content: "test"
+)
+
+worthPhotoSample = WorthPhoto.create!(
+    photo: "https://images.pexels.com/photos/157465/pexels-photo-157465.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
+)
+
+relocationPhotoSample = RelocationPhoto.create!(
+    photo: "https://images.pexels.com/photos/157465/pexels-photo-157465.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
+)
+
+marketPhotoSample = MarketReportPhoto.create!(
+    photo: "https://images.pexels.com/photos/157465/pexels-photo-157465.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
+)
+
+townSample = Town.create!(
+    bannerImage: "https://images.pexels.com/photos/111963/pexels-photo-111963.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    name: "Boston",
+    headerText1: "Boston Town",
+    headerText2: "heading here",
+    townheader: "Boston Links",
+    content: "test"
+)
+
+partnerSample = PartnerCategory.create!(
+    bannerImage: "https://images.pexels.com/photos/3760067/pexels-photo-3760067.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    name: "Lawyers",
+    headerText1: "Lawyer Friends",
+    headerText2: "Edit heading!",
+    content: "test"
+)
